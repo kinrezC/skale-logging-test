005f978cc974e8bc0216975985c7ba7a64ab3482
Kinrezc <matt.czernik@gmail.com>
2019-12-02 16:25:27 -0500

    debug

--- abi.js
+++ abi.js
@@ -15,6 +15,21 @@ module.exports = [
     type: "function"
   },
   {
+    constant: false,
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "num",
+        type: "uint256"
+      }
+    ],
+    name: "setNumber",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
     constant: true,
     inputs: [],
     name: "sayHello",
